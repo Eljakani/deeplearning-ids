a4018563bdaac46fb49f345bfbee309236b68c97
Pegasus070 <mediosme8@gmail.com>
2024-04-21 18:29:44 +0100

    try main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from dpkt.pcap import Reader
 from scapy.all import *
 from scapy.layers.l2 import Ether
+from modules import calculate_srv_diff_host_rate
 from modules import calculate_dst_bytes
-from modules import calculate_flag
+from modules import calculate_dst_host_count
+from modules import calculate_count
+from modules import calculate_diff_srv_rate
 
 
 def dpkt_to_scapy(packet_data):
@@ -18,7 +21,7 @@ def dpkt_to_scapy(packet_data):
     return Ether(packet_data)
 
 
-pcap_file_path = 'C:\\Users\\HP\\Desktop\\Doss_Partage\\pcapp.pcap'
+pcap_file_path = 'C:\\Users\\el mahdi\\Desktop\\dataset AI\\test.pcap'
 
 with open(pcap_file_path, 'rb') as f:
     pcap = Reader(f)
@@ -26,6 +29,11 @@ with open(pcap_file_path, 'rb') as f:
     timestamp, packet_data = next(pcap)
     scapy_packet = dpkt_to_scapy(packet_data)
 
-    flag = calculate_dst_bytes.calculate_dst_bytes(scapy_packet)
+    flag = calculate_count.calculate_count(pcap)
+    #flag1 = calculate_diff_srv_rate.calculate_diff_srv_rate(pcap)
+    flag3 = calculate_dst_bytes.calculate_dst_bytes(scapy_packet)
+    flag4 = calculate_srv_diff_host_rate.calculate_srv_diff_host_rate(pcap)
+    flag2 = calculate_dst_host_count.calculate_dst_host_count(pcap)
 
-print(flag)
+
+print(flag, flag2, flag3, flag4)
